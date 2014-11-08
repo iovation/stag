50cbcbc8ecc219e4ba928b4dc7bb96cd2159b573
Aaron Blew <aaron.blew@iovation.com>
2014-11-07 16:43:30 -0800

    Benchmark mode.  10,000,000 messages across 100000 seconds takes about 12.6 seconds on the laptop

--- flow.go
+++ flow.go
@@ -9,17 +9,17 @@ import (
 
 // Slice of metric data for a given period of time
 type TimeSlice struct {
-	Name	string		// Graphite name of the metric
-	Values	[]float64	// Values of events for this period
-	Epoch	uint64		// epoch of time slice (i.e. events happened here)
-	TTL		*time.Timer	// Do we need a ttl Timer
+	Name	string			// Graphite name of the metric
+	Values	[]float64		// Values of events for this period
+	Epoch	int				// epoch of time slice (i.e. events happened here)
+	TTL		*time.Timer		// TTL timer for the slice
 }
 
 // Type for a single incoming metric
 type Metric struct {
 	Name	string	// Graphite name of the metric
 	Value	float64	// Values of events for this period
-	Epoch	uint64	// epoch of time slice (i.e. events happened here)
+	Epoch	int	// epoch of time slice (i.e. events happened here)
 }
 
 // Used to create a new TimeSlice
@@ -59,40 +59,45 @@ func (c CountContents) Value() float64 {
 	return float64(len(c.values))
 }
 
-func main() {
-
+func main() {	
 	MetricsIn := make(chan *Metric)
 	
 	go func() {
 		SliceMap := make(map[string] *TimeSlice)
+		MasterSubmitTicker := time.NewTicker(time.Duration(2 * time.Second))
+
 		for {
 			select {
 			case metric := <-MetricsIn:
-				MetricKey := metric.Name + strconv.FormatUint(metric.Epoch, 10)
+				MetricKey := metric.Name + strconv.Itoa(metric.Epoch)
 				slice, present := SliceMap[MetricKey]
 				if present {
 					// Add the value to the slice
 					slice.Add(metric.Value)
-					// fmt.Println(SliceMap[MetricKey])
 				} else {
 					// Create a new slice for that value
 					SliceMap[MetricKey] = new(TimeSlice)
 					SliceMap[MetricKey].Create(metric)
-					// fmt.Println(SliceMap)
-					// fmt.Println(SliceMap[MetricKey])
 				}
-
-				a := MetricCalculator(AverageContents{values: SliceMap[MetricKey].Values})
-				fmt.Println("Average of all values is ", a.Value())
-				c := MetricCalculator(CountContents{values: SliceMap[MetricKey].Values})
-				fmt.Println("Average of all values is ", c.Value())
+			// If there's nothing coming in let's build a new map of slices to submit, then iterate and calculate them)
+			// (or just submit them in the short term)
+			case <-MasterSubmitTicker.C:
+				if len(SliceMap) == 0 { break }	// Break if there's no data yet.
+				for _, Slice := range SliceMap {
+					_ = MetricCalculator(AverageContents{values: Slice.Values})
+					// fmt.Println("Average of all values is ", a.Value())
+					_ = MetricCalculator(CountContents{values: Slice.Values})
+					// fmt.Println("Average of all values is ", strconv.FormatFloat(c.Value(), 'f', 2, 64))
+				}					
 			}
 		}
 	}()
 
+	StartTime := time.Now()
 	// Generate some test data
-	for i := 0; i <= 10; i++ {
-		testMetric := Metric{Name: "honk", Value: rand.Float64(), Epoch: 1}
+	for i := 0; i <= 10000000; i++ {
+		testMetric := Metric{Name: "honk", Value: rand.Float64(), Epoch: rand.Intn(100000)}
 		MetricsIn <- &testMetric
 	}
+	fmt.Println(time.Since(StartTime))
 }
