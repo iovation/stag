24b201f86606c85f9ec2a375b30d982da7927335
Aaron Blew <aaron.blew@iovation.com>
2014-11-10 22:30:28 -0800

    Added some complexity to (hopefully) make parallelization easier in the future

--- flow.go
+++ flow.go
@@ -1,39 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "strconv"
-    "math/rand"
+	"fmt"
+	"math/rand"
+	// "strconv"
+	"time"
 )
 
 // Slice of metric data for a given period of time
 type TimeSlice struct {
-	Name	string			// Graphite name of the metric
-	Values	[]float64		// Values of events for this period
-	Epoch	int				// epoch of time slice (i.e. events happened here)
-	TTL		*time.Timer		// TTL timer for the slice
-}
-
-// Type for a single incoming metric
-type Metric struct {
-	Name	string	// Graphite name of the metric
-	Value	float64	// Values of events for this period
-	Epoch	int	// epoch of time slice (i.e. events happened here)
+	Name   string      // Graphite name of the metric
+	Values []float64   // Values of events for this period
+	Epoch  int         // epoch of time slice (i.e. events happened here)
+	TTL    *time.Timer // TTL timer for the slice
 }
 
 // Used to create a new TimeSlice
 func (t *TimeSlice) Create(m *Metric) {
 	t.Name = m.Name
 	t.Epoch = m.Epoch
-	t.TTL = time.NewTimer(10 * time.Second) // NOTE: Keep me in sync with the time under the Add method
+	// README: Keep me in sync with the time under the Add method
+	// TODO: Make this configurable
+	t.TTL = time.NewTimer(10 * time.Second)
+	t.Values = make([]float64, 10)
 	t.Add(m.Value)
 }
 
 // Add a value to a TimeSlice
 func (t *TimeSlice) Add(v float64) {
 	t.Values = append(t.Values, v)
-	t.TTL.Reset(10 * time.Second) // NOTE: Keep me in sync with the time under the Create method
+	// NOTE: Keep me in sync with the time under the Create method
+	// TODO: Make this configurable
+	t.TTL.Reset(10 * time.Second)
 }
 
 type MetricCalculator interface {
@@ -43,6 +41,7 @@ type MetricCalculator interface {
 type AverageContents struct {
 	values []float64
 }
+
 func (a AverageContents) Value() float64 {
 	sum := float64(0.0)
 	length := len(a.values)
@@ -55,49 +54,136 @@ func (a AverageContents) Value() float64 {
 type CountContents struct {
 	values []float64
 }
+
 func (c CountContents) Value() float64 {
 	return float64(len(c.values))
 }
 
-func main() {	
-	MetricsIn := make(chan *Metric)
-	
+// A slice container that contains a ticker for iteration
+// TODO: mutex per slice container that prevents the main loop from adding metrics to the timeslice list or calculating values from the timeslice list.
+// 		Goroutine in Create will do submission handling
+type SliceContainer struct {
+	Name         string             // Graphite name of the metric
+	SliceMap     map[int]*TimeSlice // Time Slices for this metric
+	ActiveSlices map[int]int        // Set of *active* (timeslices are removed from this after they've been submitted, and re-added after new data comes in)
+	SubmitTicker *time.Ticker       // Ticker for submission
+	Input        chan *Metric       // Input channel for new Metrics
+}
+
+func (s *SliceContainer) Create(m *Metric) {
 	go func() {
-		SliceMap := make(map[string] *TimeSlice)
-		MasterSubmitTicker := time.NewTicker(time.Duration(2 * time.Second))
+		// fmt.Println("SliceContainer.Create\tInner loop starting")
+		for {
+			select {
+			case i := <-s.Input:
+				// fmt.Println("SliceContainer.Create\tGot input from inner channel")
+				s.Add(i)
+				// fmt.Println("SliceContainer.Create\tAdded value to Slice")
+				s.ActiveSlices[i.Epoch] = i.Epoch
+				// fmt.Println("SliceContainer.Create\tAdded value to active slices")
+			case <-s.SubmitTicker.C:
+				if len(s.ActiveSlices) == 0 {
+					break
+				} // Break if there's no data yet.
+
+				// Now iterate over each active slice
+				for _, Slice := range s.ActiveSlices {
+					// TODO: Handle these calculations via graphite output
+					go func() {
+						v := make([]float64, len(s.SliceMap[Slice].Values))
+						copy(v, s.SliceMap[Slice].Values)
+						_ = MetricCalculator(AverageContents{values: v})
+					}()
+					go func() {
+						v := make([]float64, len(s.SliceMap[Slice].Values))
+						copy(v, s.SliceMap[Slice].Values)
+						_ = MetricCalculator(CountContents{values: v})
+					}()
+					// fmt.Println("Average of all values is ", a.Value())
+					// fmt.Println("Count of all values is ", strconv.FormatFloat(c.Value(), 'f', 2, 64))
+
+					// Remove these slices from the active list
+					delete(s.ActiveSlices, Slice)
+				}
+				//	TODO: Implement TTL sweep here.  Maybe just do a sweep every second?
+			}
+			// fmt.Println("SliceContainer.Create\tFinished inner loop")
+		}
+	}()
+}
+
+func (s *SliceContainer) Add(m *Metric) {
+	// fmt.Println("SliceContainer.Add\tAdding Value")
+	s.SliceMap[m.Epoch].Add(m.Value)
+	// fmt.Println("SliceContainer.Add\tAdding Adding epoch to active list")
+	s.ActiveSlices[m.Epoch] = m.Epoch
+}
+
+// Type for a single incoming metric
+type Metric struct {
+	Name  string  // Graphite name of the metric
+	Value float64 // Values of events for this period
+	Epoch int     // epoch of time slice (i.e. events happened here)
+}
+
+// Random strings
+var letters = []rune("abcdefghijklmnopqrstuvwxyz")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func main() {
+	MetricsIn := make(chan *Metric)
+
+	go func() {
+		MetricMap := make(map[string]*SliceContainer)
+		// fmt.Println("Got a MetricMap")
 
 		for {
 			select {
 			case metric := <-MetricsIn:
-				MetricKey := metric.Name + strconv.Itoa(metric.Epoch)
-				slice, present := SliceMap[MetricKey]
-				if present {
-					// Add the value to the slice
-					slice.Add(metric.Value)
-				} else {
-					// Create a new slice for that value
-					SliceMap[MetricKey] = new(TimeSlice)
-					SliceMap[MetricKey].Create(metric)
+				// fmt.Println(metric.Name + "." + strconv.Itoa(metric.Epoch))
+
+				_, present := MetricMap[metric.Name]
+				if present != true {
+					// fmt.Println("Metric not present in MetricMap, creating a new SliceContainer")
+					// Create a new TimeSlice for that epoch
+					MetricMap[metric.Name] = new(SliceContainer)
+					MetricMap[metric.Name].Name = metric.Name
+					MetricMap[metric.Name].SliceMap = make(map[int]*TimeSlice)
+					MetricMap[metric.Name].ActiveSlices = make(map[int]int)
+					MetricMap[metric.Name].SubmitTicker = time.NewTicker(time.Duration(2 * time.Second))
+					MetricMap[metric.Name].Input = make(chan *Metric, 1000)
+					// fmt.Println("SliceContainer initalized")
+					MetricMap[metric.Name].Create(metric)
+					// fmt.Println("SliceContainer Created")
 				}
-			// If there's nothing coming in let's build a new map of slices to submit, then iterate and calculate them)
-			// (or just submit them in the short term)
-			case <-MasterSubmitTicker.C:
-				if len(SliceMap) == 0 { break }	// Break if there's no data yet.
-				for _, Slice := range SliceMap {
-					_ = MetricCalculator(AverageContents{values: Slice.Values})
-					// fmt.Println("Average of all values is ", a.Value())
-					_ = MetricCalculator(CountContents{values: Slice.Values})
-					// fmt.Println("Average of all values is ", strconv.FormatFloat(c.Value(), 'f', 2, 64))
-				}					
+				_, Epresent := MetricMap[metric.Name].SliceMap[metric.Epoch]
+				if Epresent != true {
+					MetricMap[metric.Name].SliceMap[metric.Epoch] = new(TimeSlice)
+					MetricMap[metric.Name].SliceMap[metric.Epoch].Create(metric)
+				}
+				// fmt.Println("Passing Input to SliceMap")
+				MetricMap[metric.Name].Input <- metric
 			}
 		}
 	}()
 
 	StartTime := time.Now()
+	fmt.Println(StartTime)
 	// Generate some test data
 	for i := 0; i <= 10000000; i++ {
-		testMetric := Metric{Name: "honk", Value: rand.Float64(), Epoch: rand.Intn(100000)}
+		testMetric := Metric{Name: randSeq(2), Value: rand.Float64(), Epoch: rand.Intn(10)}
+		// fmt.Println("Got a test metric")
 		MetricsIn <- &testMetric
+		// time.Sleep(100 * time.Millisecond)
+		// fmt.Println("Passed in testMetric")
 	}
 	fmt.Println(time.Since(StartTime))
+	time.Sleep(11 * time.Second) // Sleep 11 seconds so we have a chance to calculate and TTL
 }
